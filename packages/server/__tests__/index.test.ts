fc41aa031460ce81874dd8ff8a18733d61904fbb
Nabs <43505437+NAB5@users.noreply.github.com>
2024-07-19 02:16:50 -0400

    test: use constant for endpoint

--- packages/server/__tests__/index.test.ts
+++ packages/server/__tests__/index.test.ts
@@ -1,8 +1,10 @@
 import supertest from "supertest";
 import { expect, it } from "vitest";
 
+import { TEST_SERVER_ENDPOINT } from "@tests/lib/constants";
+
 it("should respond to GET / route", async () => {
-  const response = await supertest(`http://localhost:3000`).get("/").expect(200);
+  const response = await supertest(TEST_SERVER_ENDPOINT).get("/").expect(200);
 
   expect(response.body).toEqual({ status: "OK" });
 });
