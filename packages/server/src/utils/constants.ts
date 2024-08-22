bdb67ec1139a49f2a852c77c8b96cbe8beac6443
polarzero <0xpolarzero@gmail.com>
2024-08-22 12:00:16 +0200

    refactor: unique env variables

--- packages/server/src/utils/constants.ts
+++ packages/server/src/utils/constants.ts
@@ -1,4 +1,6 @@
+import { resolve } from "path";
 import { resourceToHex } from "@latticexyz/common";
+import { config } from "dotenv";
 import { Redis } from "ioredis";
 import RedisMock from "ioredis-mock";
 import { createWalletClient, Hex, http, publicActions } from "viem";
@@ -6,16 +8,21 @@ import { privateKeyToAccount } from "viem/accounts";
 
 import { chains } from "@/utils/chain";
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+config({ path: resolve(process.cwd(), ".env") });
+
 export const DEV = process.env.NODE_ENV === "development";
 export const PROD = process.env.NODE_ENV === "production";
 export const TEST = process.env.NODE_ENV === "test";
 
-export const SESSION_SECRET = process.env.SESSION_SECRET ?? "pqu3QS3OUB9tIiWntAEI7PkaIfp2H73Me2Lqq340FXc2";
-export const PORT = parseInt(process.env.PORT ?? "3000");
-export const CHAIN = process.env.CHAIN ?? "foundry";
+export const SESSION_SECRET =
+  process.env.GASLESS_SERVER_SESSION_SECRET ?? "pqu3QS3OUB9tIiWntAEI7PkaIfp2H73Me2Lqq340FXc2";
+export const PORT = parseInt(process.env.GASLESS_SERVER_PORT ?? "3000");
+export const CHAIN = process.env.GASLESS_SERVER_CHAIN ?? "foundry";
 
 export const SERVER_ACCOUNT = privateKeyToAccount(
-  (process.env.PRIVATE_KEY ?? "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80") as Hex,
+  (process.env.GASLESS_SERVER_PRIVATE_KEY ??
+    "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80") as Hex,
 );
 export const WALLET = createWalletClient({
   account: SERVER_ACCOUNT,
